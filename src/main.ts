02b1a8c72a6e374bc22b675407a88061b0ae968c
Abhijeet Patil <patilabhijeet.ap@gmail.com>
2023-07-02 18:32:49 +0530

    bug fixes

--- src/main.ts
+++ src/main.ts
@@ -16,8 +16,9 @@ const initApp = (): void => {
     const newEntryText = input.value.trim()
     if(!newEntryText.length) return
 
-    const itemId: number = fullList.list.length ? parseInt(fullList.list[fullList.list.length - 1].id + 1) : 1
+    const itemId: number = fullList.list.length ? parseInt(fullList.list[fullList.list.length - 1].id) + 1 : 1
     const newItem = new ListItem(itemId.toString(), newEntryText)
+    input.value = ''
     fullList.addItem(newItem)
     template.render(fullList)
   })
